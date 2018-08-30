9fcd640e4397ed6f49b50eec648169ee9d4ce1b8
Andyfock <42843263+Andyfock@users.noreply.github.com>
2018-08-31 00:13:40 +0800

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,4 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## This function makes a special matrix
 makeCacheMatrix <- function(x = matrix()) {
 	m <- NULL
         set <- function(y) {
@@ -20,7 +16,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated or/and been chanaged, then the cachesolve should retrieve the inverse 
+## from the cache.
 
 cacheSolve <- function(x, ...) {
         m <- x$getinverse()
